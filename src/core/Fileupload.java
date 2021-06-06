751c94f337ba427e8f4b00e68547faf292f29b4f
Adhrit2017 <vaishali.akhade@gmail.com>
2021-06-06 22:35:40 +0530

    updatedFiles

--- src/core/Fileupload.java
+++ src/core/Fileupload.java
@@ -1,11 +1,13 @@
 package core;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Fileupload {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver","driver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -17,8 +19,19 @@ public class Fileupload {
 		String currenturl = driver.getCurrentUrl();
 		System.out.println("url title is:" +currenturl);
 		
+		WebElement ChooseFile = driver.findElement(By.xpath("//input[@name='uploadfile_0']"));
+		ChooseFile.sendKeys("C:\\Users\\User\\Desktop\\Vaishali Doc");
+		WebElement terms = driver.findElement(By.name("terms"));
+		terms.click();
+		WebElement submit = driver.findElement(By.id("submitbutton"));
+		submit.click();
 		
 
+		String Title = driver.getTitle();
+		System.out.println("Current Website Title = " + Title);
+		Thread.sleep(10000);
+		driver.quit();
 	}
+	
 
 }
